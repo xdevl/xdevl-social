29fcfcf6fe3e8381d0763c56c1665fb4a225132c
xdevl <xdevl@users.noreply.github.com>
2015-06-10 00:48:06 +0100

    Initial commit

---
+++ xdevl-social.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: XdevL social
+ * Plugin URI: http://www.xdevl.com/blog
+ * Description: Allows users to log in using social platforms
+ * Version: 1.0
+ * Date: 09 June 2015
+ * Author: XdevL
+ * Author URI: http://www.xdevl.com/blog
+ * License: GPL2
+ * 
+ * @copyright Copyright (c) 2015, XdevL
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace xdevl\social
+{
+
+defined('ABSPATH') or die('No script kiddies please!') ;
+
+define(__NAMESPACE__.'\PLUGIN_NAMESPACE','xdevl_social') ;
+
+} //end xdevl\social
+?>
